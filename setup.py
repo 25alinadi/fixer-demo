2646983922c1e9228de5a1e1515637aa92036b2c
25alinadi <alinadi14@gmail.com>
2021-04-20 06:19:43 +0430

    Bump to version 2

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(name='fixer-demo',
-      version='0.1',
+      version='0.2',
       description='Fixer service demo package',
       url='https://github.com/25alinadi/fixer-demo.git',
       author='ali nadi',
       author_email='alinadi14@gmail.com',
       license='MIT',
       packages=['fixer'],
+      install_requires=['requests'],
       zip_safe=False)
