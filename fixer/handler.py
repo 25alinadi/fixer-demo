06cce104a8cb30989943ee3aa0c514937bc6b412
25alinadi <alinadi14@gmail.com>
2021-04-20 06:13:12 +0430

    python

---
+++ fixer/handler.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+BASE_PATH = 'http://data.fixer.io/api/latest?access_key='
+
+
+def get_rates(api_key):
+    response = requests.get(BASE_PATH + api_key)
+    if response.status_code == 200:
+        return json.loads(response.text)
+
+    return None
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='fixer-demo',
+      version='0.1',
+      description='Fixer service demo package',
+      url='https://github.com/25alinadi/fixer-demo.git',
+      author='ali nadi',
+      author_email='alinadi14@gmail.com',
+      license='MIT',
+      packages=['fixer'],
+      zip_safe=False)
